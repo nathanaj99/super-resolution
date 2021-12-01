6b85516030b7299599a0d9e99b7af53fbc1e55fd
Nathan Jo <nathanjo@sls-314-mbpro2021.attlocal.net>
2021-11-30 20:45:18 -0800

    debug

--- edsr.py
+++ edsr.py
@@ -20,7 +20,7 @@ def plot_sample(lr, sr):
     plt.figure(figsize=(20, 10))
 
     images = [lr, sr]
-    titles = ['LR', f'SR (x{sr.shape[0] // lr.shape[0]})']
+    titles = ['LR', 'SR']
 
     for i, (img, title) in enumerate(zip(images, titles)):
         plt.subplot(1, 2, i+1)
